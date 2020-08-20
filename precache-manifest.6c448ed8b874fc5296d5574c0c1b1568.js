e7001c0c0ef7e8a5ac0aa91cb9065cd4edf855db
K-Hariprasad <hariprasadk125@gmail.com>
2020-08-20 22:27:28 +0530

    Updates

---
+++ precache-manifest.6c448ed8b874fc5296d5574c0c1b1568.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5229f80484c06173d544f484588bdda2",
+    "url": "/eCommerce/index.html"
+  },
+  {
+    "revision": "1ab9d32801303c0d14a5",
+    "url": "/eCommerce/static/css/main.0f74e95d.chunk.css"
+  },
+  {
+    "revision": "85689e416828fd4abd26",
+    "url": "/eCommerce/static/js/2.5f2ce6ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eCommerce/static/js/2.5f2ce6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab9d32801303c0d14a5",
+    "url": "/eCommerce/static/js/main.85a9d1aa.chunk.js"
+  },
+  {
+    "revision": "1ff420effc4144ef0623",
+    "url": "/eCommerce/static/js/runtime-main.cb38feba.js"
+  },
+  {
+    "revision": "f8b80a4c928360dbf30546c8c7dcdec5",
+    "url": "/eCommerce/static/media/c1.f8b80a4c.jpg"
+  },
+  {
+    "revision": "b9256ec496e7d1774a459528c0369cd3",
+    "url": "/eCommerce/static/media/c2.b9256ec4.jpg"
+  },
+  {
+    "revision": "4954ae49fe0d9586cacade6f66969422",
+    "url": "/eCommerce/static/media/c3.4954ae49.jpg"
+  },
+  {
+    "revision": "5001cf5b1e8a1bf083a87fac656f6bd4",
+    "url": "/eCommerce/static/media/casualshoe.5001cf5b.jpg"
+  },
+  {
+    "revision": "84fe61f4053bf2223a0e4c91271feef0",
+    "url": "/eCommerce/static/media/fc.84fe61f4.jpg"
+  },
+  {
+    "revision": "502bb5499ccb9cc9dc3a2e9a991f9004",
+    "url": "/eCommerce/static/media/kurta.502bb549.jpg"
+  },
+  {
+    "revision": "d1aa76bb9df9008de13992b05a0c72f0",
+    "url": "/eCommerce/static/media/shirt.d1aa76bb.jpg"
+  },
+  {
+    "revision": "f5de65054f25600cd03260b30681f2eb",
+    "url": "/eCommerce/static/media/t.f5de6505.jpg"
+  },
+  {
+    "revision": "059c93046f2133fe9a3dfc14f12cf53c",
+    "url": "/eCommerce/static/media/top.059c9304.jpg"
+  }
+]);
